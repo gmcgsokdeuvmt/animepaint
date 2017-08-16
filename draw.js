f8fef0ce5d03a06b5eac4d8964e94f887856e0c4
ran <mochimochi.morst@gmail.com>
2017-08-16 22:31:53 +0900

    amend

--- draw.js
+++ draw.js
@@ -45,17 +45,11 @@ function generateGIF() {
         gif.addFrame(canvases[key],{delay: 1000/fps});        
     }
     gif.on('finished', function(blob) {
+
         var reader = new FileReader();
-        reader.onload = function(e) {
-
-            const a = document.createElement('a');
-            a.href = e.target.result;
-            a.download = 'download.gif';
-            a.target = '_blank';
-
-            a.click();
-        }
+        reader.onload = function(e) {window.open(e.target.result);}
         reader.readAsDataURL(blob);
+
         console.log('finished');
         
     });
